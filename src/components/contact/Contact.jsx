e814b62046a0f4213b00da75dde5eba3b7a24ad9
Prajwal P <prajwalpr2@gmail.com>
2024-04-15 09:15:25 +0530

    remove dark color

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div id="contact" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="text-center">
         
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white">
           <div className="py-8 pb-0 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
             <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Your voice matters. Reach out for support, feedback, or inquiries about our services and plans.</p>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
             scrollToSection(item.name.toLowerCase());
           }}
           href={`#${item.name}`} 
-          className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 "
+          className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:text-white md:dark:hover:bg-transparent "
         >
           {item.name}
         </a>
@@ -67,7 +67,7 @@ const Header = () => {
   };
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-10" alt="Care-Taker Logo" />
@@ -79,7 +79,7 @@ const Header = () => {
 
           <button data-collapse-toggle="navbar-sticky" 
           type="button" 
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600" 
           aria-controls="navbar-sticky" 
           aria-expanded="false" 
           onClick={toggleMenu}>
