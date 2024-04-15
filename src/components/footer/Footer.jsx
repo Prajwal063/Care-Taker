c64c432445f944ee7d6571d66ff04f1bb2c8f5dd
Prajwal P <prajwalpr2@gmail.com>
2024-04-15 09:20:26 +0530

    remove contact dark

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/care taker logo.png"
 
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <footer className="bg-white rounded-lg shadow  m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
               <a href="/" className="flex items-center justify-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse ">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -90,7 +90,7 @@ const Header = () => {
           </button>
         </div>
         <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:border-gray-700">
             {renderNavItems(navItems)}
           </ul>
         </div>
