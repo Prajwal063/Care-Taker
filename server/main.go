4bab8d060655e5f2a341d097960cf6b19a86a013
Varun Athreya <varun.athreya@contentstack.com>
2024-06-09 22:21:14 +0530

    added auth store

--- server/main.go
+++ server/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"care-taker/database"
 	"care-taker/initializers"
 	"care-taker/routes"
 
 	helmet "github.com/danielkov/gin-helmet"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,14 +24,24 @@ func init() {
 
 func main() {
 	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // Adjust as needed
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 	router.Use(gin.ErrorLogger())
 	router.Use(helmet.Default())
+	router.Use(sessions.Sessions("session", initializers.AuthStore()))
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
 
 	router.GET("/", func(c *gin.Context) {
@@ -37,10 +50,11 @@ func main() {
 		})
 	})
 
+	routes.AuthRoutes(router.Group("/auth"))
 	routes.ServiceRoutes(router.Group("/service"))
 	routes.EventRoutes(router.Group("/event"))
 
-	err := router.Run(":" + port)
+	err := router.Run("localhost:" + port)
 	if err != nil {
 		log.Fatal(err)
 	}
